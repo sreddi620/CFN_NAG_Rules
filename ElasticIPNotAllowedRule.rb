7a5b428c592b9885040b9f5c9736ca86f7d3471a
sreddi620 <reddi.satish@gmail.com>
2019-08-12 11:09:10 -0600

    Rule to identify CFN template with Elastic IP

---
+++ ElasticIPNotAllowedRule.rb
@@ -0,0 +1,25 @@
+require 'cfn-nag/violation'
+require_relative 'boolean_base_rule'
+
+class ElasticIPNotAllowedRule < BooleanBaseRule
+
+  def rule_text
+    'Elastic IP should not be assigned'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'WU1'
+  end
+
+  def resource_type
+    'AWS::EC2::EIP' || 'AWS::EC2::EIPAssociation'
+  end
+
+  def boolean_property
+    :EIP
+  end
+end
