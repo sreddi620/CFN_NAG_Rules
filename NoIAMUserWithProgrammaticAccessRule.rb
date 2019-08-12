1d6681db27120105452703e09d527c5279ebadb2
sreddi620 <reddi.satish@gmail.com>
2019-08-12 11:19:18 -0600

    Rule to identify user with programmatic access

---
+++ NoIAMUserWithProgrammaticAccessRule.rb
@@ -0,0 +1,25 @@
+require 'cfn-nag/violation'
+require_relative 'boolean_base_rule'
+
+class NoIAMUserWithProgrammaticAccessRule < BooleanBaseRule
+
+  def rule_text
+    'User should not be provided a Programmatic access'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'WU3'
+  end
+
+  def resource_type
+    'AWS::IAM::AccessKey'
+  end
+
+  def boolean_property
+    :AccessKey
+  end
+end
